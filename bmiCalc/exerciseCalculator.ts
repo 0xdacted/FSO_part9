9cc3540f98959e0c4f9fa989337d54616ee6900c
Red <crabmarket56@gmail.com>
2023-03-10 13:43:23 +0900

    Setup interface

---
+++ bmiCalc/exerciseCalculator.ts
@@ -0,0 +1,9 @@
+interface ExerciseResult {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
