cad8a90824428a0232cbcdad88e9a4ac315cdb7b
Red <crabmarket56@gmail.com>
2023-03-10 16:21:26 +0900

    Add exercises post endpoint

--- bmiCalc/index.ts
+++ bmiCalc/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
@@ -22,6 +23,31 @@ app.get('/bmi', (req, res) => {
   });
 });
 
+app.post('/exercises', (req, res) => {
+  const { daily_exercises } = req.body;
+  if (!daily_exercises || daily_exercises.length < 2) {
+    return res.status(400).json({ error: "parameters missing" });
+  }
+  
+  const target = Number(daily_exercises[0]);
+
+  if (isNaN(target) || target <= 0 || !Array.isArray(daily_exercises.slice(1)) || 
+  daily_exercises.slice(1).some(value => typeof value !== 'number')) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+  const result = calculateExercises(daily_exercises.slice(1), target);
+
+  return res.json({
+    periodLength: result.periodLength,
+    trainingDays: result.trainingDays,
+    success: result.success,
+    rating: result.rating,
+    ratindDescription: result.ratingDescription,
+    target: result.target,
+    average: result.average,
+  });
+
+});
 const PORT = 3003;
 
 app.listen(PORT, () => {
