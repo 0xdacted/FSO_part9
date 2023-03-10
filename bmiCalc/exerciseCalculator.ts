05c6c1c761bbd31aa4508f2c732e5a6cbe77b91e
Red <crabmarket56@gmail.com>
2023-03-10 13:49:44 +0900

    Create calculateExercises function

--- bmiCalc/exerciseCalculator.ts
+++ bmiCalc/exerciseCalculator.ts
@@ -7,3 +7,25 @@ interface ExerciseResult {
   target: number,
   average: number
 }
+
+const calculateExercises = (dailyExerciseHours: Array<number>, target: number): ExerciseResult => {
+  const periodLength = dailyExerciseHours.length
+  const trainingDays = dailyExerciseHours.filter(hours => hours > 0).length
+  const totalHours = dailyExerciseHours.reduce((sum, hours) => sum + hours, 0)
+  const average = totalHours / periodLength
+  const success = average >= target
+  const rating = success ? 3 : average >= target - 0.5 ? 2 : 1
+  const ratingDescription = rating === 3 ? 'excellent' : rating === 2 ? 'not too bad but could be better' : 'poor'
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
