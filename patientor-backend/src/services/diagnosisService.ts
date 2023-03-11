50f94ae43f1c3610378cb31cc401c18b333d3908
Red <crabmarket56@gmail.com>
2023-03-11 15:27:12 +0900

    Add typing to data

---
+++ patientor-backend/src/services/diagnosisService.ts
@@ -0,0 +1,12 @@
+import diagnosesData from "../../data/diagnoses";
+import { Diagnoses } from "../types";
+
+const diagnoses: Diagnoses = diagnosesData
+const getDiagnoses = () => {
+  return diagnoses
+}
+
+
+export default {
+  getDiagnoses
+}
