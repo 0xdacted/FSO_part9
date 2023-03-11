bf73f39c786a2f9156d6eec375b6186edc3c6f03
Red <crabmarket56@gmail.com>
2023-03-11 20:46:13 +0900

    Fix typing

--- patientor-backend/src/utils.ts
+++ patientor-backend/src/utils.ts
@@ -1,6 +1,6 @@
 import { NewPatient, Gender } from "./types";
 
-const isString = (text: any): text is string => {
+const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
 };
 
@@ -8,40 +8,40 @@ const isDate = (date: string): boolean => {
   return Boolean(Date.parse(date));
 };
 
-const isGender = (gender: any): gender is Gender => {
+const isGender = (gender: unknown): gender is Gender => {
   return Object.values(Gender).includes(gender);
 };
 
-const parseString = (field: string, value: any): string => {
+const parseString = (field: string, value: unknown): string => {
   if (! value || !isString(value)) {
-    throw new Error(`Invalid or missing ${field}`)
+    throw new Error(`Invalid or missing ${field}`);
   }
   return value;
 };
 
-const parseDate = (date: any): string => {
+const parseDate = (date: unknown): string => {
   if (!date || !isString(date) || !isDate(date)) {
     throw new Error(`Invalid or missing date of birth`);
   }
   return date;
 };
 
-const parseGender = (gender: any): Gender => {
+const parseGender = (gender: unknown): Gender => {
   if (!gender || !isGender(gender)) {
     throw new Error(`Invalid or missing gender`);
   }
   return gender;
 };
 
-const toNewPatient = (object: any): newPatient => {
-  const newPatient: NewPatient = {
+const toNewPatient = (object: unknown): NewPatient => {
+  const addedPatient: NewPatient = {
     name: parseString('name', object.name),
     ssn: parseString('ssn', object.ssn),
     dateOfBirth: parseDate(object.dateOfBirth),
     gender: parseGender(object.gender),
     occupation: parseString('occupation', object.occupation)
   };
-  return newPatient;
+  return addedPatient;
 };
 
 export default toNewPatient;
