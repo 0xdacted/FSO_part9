3d238704dbeae32ce1663d8000c05ddf75463e0a
Red <crabmarket56@gmail.com>
2023-03-11 16:07:42 +0900

    Fix lint bug

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -13,8 +13,8 @@ app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-app.use('/api/diagnoses', diagnosesRouter)
-app.use('/api/patients', patientsRouter)
+app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
