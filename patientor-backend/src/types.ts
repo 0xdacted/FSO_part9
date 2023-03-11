763248aac2496f511b106d95d5a24f5ac919f5b4
Red <crabmarket56@gmail.com>
2023-03-11 16:21:45 +0900

    Add patient type and interface

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -1,7 +1,16 @@
-export interface Diagnosis {
+interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
 }
 
+interface Patient {
+  id: string;
+  name: string;
+  ssn: string;
+  gender: string;
+  occupation: string;
+}
+
 export type Diagnoses = Array<Diagnosis>;
+export type NonSensitivePatients = Omit<Patient, 'ssn'>;
